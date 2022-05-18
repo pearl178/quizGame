08f859825e44f9e00553e9ddf7bf4b796c90c018
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-05-18 14:01:56 -0600

    Decoded HTML entities in Python string

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,12 +4,14 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
         self.score = 0
+        self.current_question = None
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
-        user_answer = input(f"Q.{self.question_number + 1}: {current_question.text}(True/False): ")
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        self.check_answer(user_answer, current_question.answer)
+        self.current_question.text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
+        self.check_answer(user_answer)
 
     def still_has_question(self):
         return self.question_number < len(self.question_list)
