d3a95f1998a1056cf9c4f71f1c75122fdf8672a8
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-05-18 18:07:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from data import question_data
 from question_model import Question
+from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
-    question_bank.append(Question(question['question'], question['correct_answer']))
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_question():
-    quiz.next_question()
-print(f"You've completed the quiz!\nYour final score is {quiz.score}/{len(quiz.question_list)}.")
+
+quiz_ui = QuizInterface(quiz)
